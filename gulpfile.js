b5e61d121555f2c3f8ac26dd8993f9af95b8cca8
malun <malun666@126.com>
2018-06-20 15:18:19 +0800

    :boom:added the js rev

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
 // 以下代码会执行在node环境下。
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const concat = require('gulp-concat')
+const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
 const sourcemap = require('gulp-sourcemaps');
 const rev = require('gulp-rev');
 const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
+const uglify = require('gulp-uglify');
 
 // 参数： 第一个参数是任务的名字 第二个参数： 可以省略，依赖的任务名。数组类型，里面是字符串。 第三个参数： 回调函数，接受参数，任务执行完之后可调用
 // 回调函数： 返回值要么是  stream、promise、调用cb
-gulp.task('html', function () {
-  console.log('html 压缩！')
+gulp.task('html', function() {
+  console.log('html 压缩！');
 });
 
 /*
@@ -23,7 +25,7 @@ gulp.task('html', function () {
 * 5. 如果是开发阶段，需要增加sourcemap
 * 6. 给最后的main.css文件添加版本号。
 */
-gulp.task('style:dev', function () {
+gulp.task('style:dev', function() {
   return gulp
     .src(['./src/styles/**/*.{css,scss}', '!./src/styles/main.css'])
     .pipe(sourcemap.init())
@@ -42,7 +44,7 @@ gulp.task('style:dev', function () {
 });
 
 // 最终部署产品用的。
-gulp.task('style', function () {
+gulp.task('style', function() {
   return gulp
     .src(['./src/styles/**/*.{css,scss}', '!./src/styles/main.css'])
     .pipe(sass().on('error', sass.logError)) // 对请求的流中的scss代码进行编译成css代码
@@ -68,17 +70,17 @@ gulp.task('style', function () {
 });
 
 // 把assets目录中的所有文件拷贝到 dist目录
-gulp.task('copyAssets', function () {
+gulp.task('copyAssets', function() {
   return gulp.src(['./src/assets/**/*.*'], {read: true}).pipe(gulp.dest('./dist/assets/'));
 });
 
-//创建一个gulp的任务。
-gulp.task('default', ['html'], function () {
+// 创建一个gulp的任务。
+gulp.task('default', ['html'], function() {
   console.log('----gulp dfualt task');
 });
 
 // 图片压缩
-gulp.task('imagemin', function () {
+gulp.task('imagemin', function() {
   return gulp
     .src('src/assets/**/*.{jpg,png,gif,jpeg,ico}')
     .pipe(imagemin({
@@ -93,19 +95,25 @@ gulp.task('imagemin', function () {
 });
 
 // js处理
-gulp.task('js', function () {
+gulp.task('js', function() {
   // 1. es6进行代码的转换
   // 2. eslint js代码进行格式化的校验。
   // 3. js进行压缩
   // 4. js要进行打版本号（dist）
   return gulp
     .src(['src/js/**/*.js'])
+    .pipe(eslint()) // 进行校验
+    .pipe(eslint.format()) // 错误消息进行格式化输出
+    .pipe(eslint.failAfterError()) // 如果校验失败，结束当前的任务。
     .pipe(babel()) // babel
-    .pipe(gulp.dest('dist/js/'));
-
+    .pipe(uglify())
+    .pipe(rev())
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(rev.manifest())
+    .pipe(gulp.dest('src/js/'));
 });
 
-gulp.task('dev', function () {
+gulp.task('dev', function() {
   // 监控scss或者css变化，并自动调用style样式生成工作流 监控的路径不要写 ./ 不然不能监控到 添加文件的变化。
   gulp.watch([
     'src/styles/css/**', 'src/styles/scss/**'
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,10 @@
+var a,
+  b,
+  c;
 [a, b, c] = [1, 2, 3];
+var d = 'ss';
+
+c += d;
 console.log(a);
 console.log(b);
 console.log(c);
